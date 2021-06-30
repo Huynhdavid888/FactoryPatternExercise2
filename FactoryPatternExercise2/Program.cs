ce79d753279b15f81208181629feb84964827a2a
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-30 01:19:31 -0700

    initial commit

---
+++ FactoryPatternExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactoryPatternExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which database would you like to use?");
+            Console.WriteLine("Type: sql");
+            Console.WriteLine("Type: mongo");
+            Console.WriteLine("Type: list");
+
+            var userInput = Console.ReadLine();
+           IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+           var products = db.LoadData();
+            db.SaveData();
+
+            foreach(var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name} Price: {product.Price} ");
+            }
+        }
+    }
+}
